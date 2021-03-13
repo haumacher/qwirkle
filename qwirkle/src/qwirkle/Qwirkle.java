2bdac78738fb2ed7f01cf4676c897a0775b5dfde
burkhardh <burkhardh@microstar-PC.fritz.box>
2021-03-13 17:55:09 +0100

    Versuch, die Steine als Objekt anzulegen ;-)

--- qwirkle/src/qwirkle/Qwirkle.java
+++ qwirkle/src/qwirkle/Qwirkle.java
@@ -3,6 +3,20 @@ package qwirkle;
 public class Qwirkle {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Burkhard!!!");
+		System.out.println("Quirkle trial!!!");
+		//class stein [string farbe, string form];
+		object steine= array of [string farbe, string form];
+		var farben = ["yellow", "orange", "red", "blue", "green", "magenta"];
+		var formen = ["circle", "square", "star", "pounce", "cross", "diamond"];
+		for var i=1-3{
+				for var j=1-6{
+					for var k=1-6{
+						steine[j+6*k+36*i]=[farben[j], formen[k]]
+					}
+				}
+		}		
+				
+		num i;
+		
 	}
 }
