fc5c9122b6777ec53f8c373bbe4378e56f2c07c8
burkhardh <burkhardh@microstar-PC.fritz.box>
2021-03-13 17:59:35 +0100

    kleine Fehlerkorrektur

--- qwirkle/src/qwirkle/Qwirkle.java
+++ qwirkle/src/qwirkle/Qwirkle.java
@@ -5,13 +5,13 @@ public class Qwirkle {
 	public static void main(String[] args) {
 		System.out.println("Quirkle trial!!!");
 		//class stein [string farbe, string form];
-		object steine= array of [string farbe, string form];
+		var steine= array of {string farbe, string form};
 		var farben = ["yellow", "orange", "red", "blue", "green", "magenta"];
 		var formen = ["circle", "square", "star", "pounce", "cross", "diamond"];
-		for var i=1-3{
-				for var j=1-6{
-					for var k=1-6{
-						steine[j+6*k+36*i]=[farben[j], formen[k]]
+		for var i=0-3{
+				for var j=0-5{
+					for var k=0-5{
+						steine[j+6*k+36*i]=[farben[j], formen[k]];
 					}
 				}
 		}		
